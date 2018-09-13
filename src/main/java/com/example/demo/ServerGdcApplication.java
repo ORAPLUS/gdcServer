86fea0cbf2a60b9e3c69c59134c25d7fe001e111
YOUB AYOUB <sis.ayoub.youb@gmail.com>
2018-09-13 22:16:56 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServerGdcApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGdcApplication.class, args);
+	}
+}
